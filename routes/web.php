d209752b5142cc3a72c724ebb1eff4029e2a1585
farrelabennaya <abennayafarrel@gmail.com>
2025-03-01 14:23:59 +0700

    Index Update Commit

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\PenerbitController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::resource('kategori', KategoriController::class);
